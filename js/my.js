185272aa6b2aa4afcfaafc0099835aaac6c43002
Alessio Lusa <alessiolusa@gmail.com>
2024-05-09 09:28:38 +0000

    adding php and changed js for php receiving data

--- js/my.js
+++ js/my.js
@@ -1,10 +1,19 @@
-$(document).ready(()=>{
+$(document).ready(() => {
     $("#btn").click(myFun)
 })
 
-function myFun(){
+function myFun() {
     testo = $("#test").val()
     numero = $("#num").val()
 
-    alert(testo + "---" + numero)
+    $.ajax({
+        url: "try.php",
+        data: {
+            primo: testo,
+            secondo: numero
+        },
+        success: function (result) {
+            console.log(result)
+        }
+    })
 }
